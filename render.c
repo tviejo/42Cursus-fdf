cb3914476b60c7aed692c17403b7361d32c0da29
Thomas Viejo <tviejo@paul-f4Br7s2.clusters.42paris.fr>
2024-05-28 20:55:19 +0200

    cleaner line creation

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: tviejo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:09:56 by tviejo            #+#    #+#             */
-/*   Updated: 2024/05/28 14:51:25 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/05/28 20:52:00 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ void    img_pix_put(t_img *img, int x, int y, int color)
 
 int render_line(t_img *img, t_line line)
 {
-        double deltaX = line.x2 - line.x1; // 10
-        double deltaY = line.y2 - line.y1; // 0
+        double deltaX = line.x2 - line.x1;
+        double deltaY = line.y2 - line.y1;
         int pixels = sqrt((deltaX * deltaX) + (deltaY * deltaY));
-        deltaX /= pixels; // 1
-        deltaY /= pixels; // 0
+        deltaX /= pixels;
+        deltaY /= pixels;
         double pixelX = line.x1;
         double pixelY = line.y1;
         while (pixels)
@@ -65,27 +65,40 @@ void    render_background(t_img *img, int color)
         }
 }
 
+void	ft_create_line(int i, int j, t_map map, t_data *data, int mode)
+{
+	t_2dcoor        end;
+        t_2dcoor        begin;
+
+	if (mode == 1)
+	{
+		begin = convertortho(i, j, atoi(map.map[i][j]), data);
+		end = convertortho(i + 1, j, atoi(map.map[i + 1][j]), data);
+		render_line(&data->img, (t_line){ begin.x, begin.y, end.x, end.y, RED_PIXEL});
+	}
+	else
+	{
+		begin = convertortho(i, j, atoi(map.map[i][j]), data);
+		end = convertortho(i, j + 1, atoi(map.map[i][j + 1]), data);
+		render_line(&data->img, (t_line){begin.x, begin.y, end.x, end.y, RED_PIXEL});	
+	}
+}
+
 int     render(t_data *data)
 {
-        t_2dcoor        end;
-        t_2dcoor        begin;
         t_map           map;
-
         int     i;
         int     j;
 	
         render_background(&data->img, WHITE_PIXEL);
         map = create_map(data->arg);
-
         i = 0;
         while (i < map.x - 1)
         {
                 j = 0;
                 while (j < map.y)
                 {
-                        begin = convertortho(i, j, atoi(map.map[i][j]), data);
-                        end = convertortho(i + 1, j, atoi(map.map[i + 1][j]), data);
-                       render_line(&data->img, (t_line){ begin.x, begin.y, end.x, end.y, RED_PIXEL});
+			ft_create_line(i, j, map, data, 1);
                         j++;
                 }
         i++;
@@ -96,9 +109,7 @@ int     render(t_data *data)
                 i = 0;
                 while (i < map.x)
                 {
-                        begin = convertortho(i, j, atoi(map.map[i][j]), data);
-                        end = convertortho(i, j + 1, atoi(map.map[i][j + 1]), data);
-                        render_line(&data->img, (t_line){begin.x, begin.y, end.x, end.y, RED_PIXEL});
+			ft_create_line(i ,j, map, data, 2); 
                         i++;
                 }
         j++;
